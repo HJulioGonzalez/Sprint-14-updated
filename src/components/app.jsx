b9aec1fe4f7dc7b500eb8b8c219125838ce2ed18
naizirhabib <naizir68@gmail.com>
2026-01-19 08:08:12 +1030

    initial phase

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'preact/hooks'
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Habib julio testing Crypto-Duck</h1>
+    </>
+  )
+}
